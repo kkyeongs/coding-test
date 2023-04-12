0b5aaeac31ba3ea69cf7b63348bc2ccdd641b74a
kkyeongs <wh___holic@nvaer.com>
2023-04-12 23:47:26 +0900

    no message

---
+++ study/src/baekjoon/Baekjoon1269.java
@@ -0,0 +1,42 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Baekjoon1269 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		Set<Integer> setA = new HashSet<>();
+		Set<Integer> setB = new HashSet<>();
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < A; i++) {
+			setA.add(Integer.parseInt(st.nextToken()));
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < B; i++) {
+			setB.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int cnt = 0;
+		for (int num : setA) {
+			if(!setB.contains(num)) cnt++;
+		}
+		
+		for (int num : setB) {
+			if(!setA.contains(num)) cnt++;
+		}
+	
+		System.out.println(cnt);
+	}
+}
